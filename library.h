3884df532a8471d8929a9d5311413ff2b9244a1f
dhianeifar <dhia.neifar@gmail.com>
2025-10-26 22:05:32 +0100

    First commit with main

---
+++ library.h
@@ -0,0 +1,6 @@
+#ifndef UTILITIES_LIBRARY_H
+#define UTILITIES_LIBRARY_H
+
+void hello(void);
+
+#endif // UTILITIES_LIBRARY_H
---
+++ main.c
@@ -0,0 +1,12 @@
+//
+// Created by Dhia Neifar on 10/26/25.
+//
+
+#include <stdio.h>
+
+#include "library.h"
+
+int main(void) {
+    printf("Hola, world!\n");
+    return 0;
+}
